b256914be910cea35be9580059fb08faba17ba0a
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2020-12-08 17:44:14 +0100

    HIP-SAXPY added

---
+++ HIP-SAXPY/Main.cpp
@@ -0,0 +1,65 @@
+#include <hip/hip_runtime.h>
+
+#include <cstddef>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <execution>
+
+__global__
+void saxpy(float a, float* x, float* y)
+{
+    size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+    y[tid] = a * x[tid] + y[tid];
+}
+
+int main()
+{
+    constexpr std::size_t num_threads = 128;
+    constexpr std::size_t num_blocks = 32;
+    constexpr std::size_t N = num_threads * num_blocks;
+
+    hipDeviceProp_t prop;
+    hipGetDeviceProperties(&prop, 0);
+    std::cout << "Device name: " << prop.name << std::endl;
+
+    const float a = 2.f;
+    std::vector<float> x(N);
+    std::vector<float> y(N);
+
+    auto prng = [engine = std::default_random_engine{},
+                 distribution = std::uniform_real_distribution<float>{ -1.f, 1.f }]() mutable {
+        return distribution(engine);
+    };
+
+    std::generate_n(x.begin(), N, prng);
+    std::generate_n(y.begin(), N, prng);
+
+    float* x_dev;
+    float* y_dev;
+
+    hipMalloc((void**)&x_dev, sizeof(float) * N);
+    hipMalloc((void**)&y_dev, sizeof(float) * N);
+
+    hipMemcpy(x_dev, x.data(), x.size() * sizeof(float), hipMemcpyHostToDevice);
+    hipMemcpy(y_dev, y.data(), y.size() * sizeof(float), hipMemcpyHostToDevice);
+
+    hipLaunchKernelGGL(saxpy, dim3(num_blocks), dim3(num_threads), 0, 0, a, x_dev, y_dev);
+
+    std::transform(std::execution::par_unseq, x.cbegin(), x.cbegin(), y.cbegin(), x.begin(),
+        [=](const float& x, const float& y){ return a * x + y; }
+    );
+
+    hipMemcpy(y.data(), y_dev, y.size() * sizeof(float), hipMemcpyDeviceToHost);
+
+    if (std::equal(x.cbegin(), x.cend(), y.cbegin()))
+        std::cerr << "Validation failed.";
+    else
+        std::cout << "Validation passed.";
+
+    hipFree(x_dev);
+    hipFree(y_dev);
+
+    return 0;
+}
